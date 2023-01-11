14055d57f07626b8422a439fbb62d8198fd68c55
杨方红 <484150040@qq.com>
2023-01-11 14:58:11 +0800

    维护信息数据，DH接口

---
+++ src/main/java/com/hm/digital/equipment/dahua/feign/ConfigsFeignBiz.java
@@ -0,0 +1,21 @@
+package com.hm.digital.equipment.dahua.feign;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hm.digital.inface.entity.Config;
+
+@FeignClient(value = "nacos", path = "/digital")
+public interface ConfigsFeignBiz {
+
+  @GetMapping(value = "/save")
+  void save(@RequestBody Config config);
+
+
+  @RequestMapping("/configList")
+  List<Config> configList(Config config);
+}
